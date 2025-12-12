b95dd97c9504da318e293d8761add62c8c790c6c
Udhay M <msudhay2005@gmail.com>
2025-12-12 19:10:01 +0530

    Replace Udhay branch with new project code

--- Backend/TriLink/DTOs/DashboardStatsDto.cs
+++
@@ -1,9 +0,0 @@
-namespace TriLink.DTOs
-{
-    public class SupplierDashboardStatsDto
-    {
-        public int TotalActiveProducts { get; set; }
-        public int OngoingOrders { get; set; }
-        public int CompletedOrders { get; set; }
-    }
-}
